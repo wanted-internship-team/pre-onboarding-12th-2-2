66139db106bec8beac8f9ba890807a173834a2a7
Dasom Park <dev.somda@gmail.com>
2023-08-31 15:55:59 +0900

    FEAT: 메인 페이지 구현
    Co-authored-by: Younseop Lee <devseop@users.noreply.github.com>
    Co-authored-by: Nor <mihyunLee@users.noreply.github.com>
    Co-authored-by: Den <sjuhan123@users.noreply.github.com>
    Co-authored-by: Joice(Hyunjin Kim) <JJineu@users.noreply.github.com>

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,75 +1,61 @@
-import React, { ChangeEvent, useState } from 'react';
+import React, { ChangeEvent, useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import useFetch from '../hooks/useFetch';
+import { getRepo } from '../apis/requests';
+import { IrepoResponse, IrepoInfo } from '../types/issue';
+import { useRepoContext } from '../contexts/repoContext';
+import Spinner from '../components/common/Spinner';
+import { STATUS, PATH } from '../constants';
 
 export default function Main() {
+  const navigate = useNavigate();
   const [values, setValues] = useState({ owner: '', repo: '' });
+  const { setRepoInfo } = useRepoContext();
 
   const setInputValue = (e: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
     setValues({ ...values, [name]: value });
   };
+
+  const { data, status, errorMessage, fetch } = useFetch<IrepoResponse>();
+
+  const validateRepo = () => {
+    fetch({
+      callback: () => getRepo(values.owner, values.repo),
+    });
+  };
+
+  useEffect(() => {
+    if (data && status === STATUS.SUCCESS) {
+      setRepoInfo({
+        owner: data.owner.login,
+        repo: data.name,
+      });
+      navigate(PATH.ISSUES);
+    }
+  }, [data, status]);
+
+  const navigateDefaultPage = () => {
+    setRepoInfo({
+      owner: 'facebook',
+      repo: 'react',
+    });
+    navigate(PATH.ISSUES);
+  };
+
   return (
     <div>
       <input type='text' placeholder='organization' name='owner' onChange={setInputValue} />
       <input type='text' placeholder='repository' name='repo' onChange={setInputValue} />
 
-      {/* <Button
-        buttonContent={status === STATUS.LOADING ? <Spinner type='button' /> : '이슈 보러가기'}
-        type='primary'
-        onClick={validateRepo}
-      /> */}
+      <button type='button' onClick={validateRepo}>
+        {status === STATUS.LOADING ? <Spinner type='button' /> : '이슈 보러가기'}
+      </button>
+      {errorMessage && <p>{errorMessage}</p>}
 
-      <button type='button'>Default Repo로 가기~! ^_^</button>
+      <button type='button' onClick={navigateDefaultPage}>
+        Default Repo로 이동
+      </button>
     </div>
   );
 }
-
-// export default function Main() {
-//   const navigate = useNavigate();
-//   const { data, status, requestOctokit } = useOctokit<IRepoResponse>();
-//   const { setRepoInfo } = useRepoContext();
-//   const [values, setValues] = useState({ owner: '', repo: '' });
-
-//   const setInputValue = (e: ChangeEvent<HTMLInputElement>) => {
-//     const { name, value } = e.target;
-//     setValues({ ...values, [name]: value });
-//   };
-
-//   const validateRepo = () => {
-//     requestOctokit({
-//       endpoint: `GET /repos/${values.owner}/${values.repo}`,
-//       body: {
-//         owner: values.owner,
-//         repo: values.repo,
-//         headers: {
-//           'X-GitHub-Api-Version': '2022-11-28',
-//         },
-//       },
-//       isGetData: true,
-//     });
-//   };
-
-//   useEffect(() => {
-//     if (data && status === STATUS.SUCCESS) {
-//       setRepoInfo({
-//         owner: data.owner.login,
-//         repo: data.name,
-//       });
-//       navigate(URL.Issues);
-//     }
-//   }, [data, status]);
-
-//   return (
-//     <Layout title='Issues'>
-//       <S.MainBox>
-//         <input type='text' placeholder='organization' name='owner' onChange={setInputValue} />
-//         <input type='text' placeholder='repository' name='repo' onChange={setInputValue} />
-//         <Button
-//           buttonContent={status === STATUS.LOADING ? <Spinner type='button' /> : '이슈 보러가기'}
-//           type='primary'
-//           onClick={validateRepo}
-//         />
-//         {status === STATUS.ERROR && <S.AlertNotice>존재하지 않는 레포지토리입니다.</S.AlertNotice>}
-//       </S.MainBox>
-//     </Layout>
-//   );
-// }
