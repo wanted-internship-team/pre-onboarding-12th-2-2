f271f36766430f2e0e7366993fcff096812c5b95
Dasom Park <dev.somda@gmail.com>
2023-08-31 15:51:01 +0900

    FEAT: api 응답 타입 지정
    Co-authored-by: Younseop Lee <devseop@users.noreply.github.com>
    Co-authored-by: Nor <mihyunLee@users.noreply.github.com>
    Co-authored-by: Den <sjuhan123@users.noreply.github.com>
    Co-authored-by: Joice(Hyunjin Kim) <JJineu@users.noreply.github.com>

--- src/apis/requests.ts
+++ src/apis/requests.ts
@@ -1,13 +1,22 @@
 import { octokitInstance } from './instance';
+import { OctokitResponse } from '@octokit/types';
+import { IrepoResponse, IissueList, IissueDetail } from '../types/issue';
 
 const baseURL = `/repos`;
 
-export const getRepo = async (owner: string, repo: string) => {
+export const getRepo = async (
+  owner: string,
+  repo: string,
+): Promise<OctokitResponse<IrepoResponse>> => {
   const res = await octokitInstance.request(`GET ${baseURL}/${owner}/${repo}`);
   return res;
 };
 
-export const getRepoIssueList = async (owner: string, repo: string, page: number) => {
+export const getRepoIssueList = async (
+  owner: string,
+  repo: string,
+  page: number,
+): Promise<OctokitResponse<IissueList>> => {
   const res = await octokitInstance.request(`GET ${baseURL}/${owner}/${repo}/issues`, {
     state: 'open',
     sort: 'comments',
@@ -17,7 +26,11 @@ export const getRepoIssueList = async (owner: string, repo: string, page: number
 };
 
 // FIXME: 요청 url 수정
-export const getIssueDetail = async (owner: string, repo: string, issueNumber: number) => {
+export const getIssueDetail = async (
+  owner: string,
+  repo: string,
+  issueNumber: number,
+): Promise<OctokitResponse<IissueDetail>> => {
   const res = await octokitInstance.request(`GET ${baseURL}/${owner}/${repo}/${issueNumber}`);
   return res;
 };
