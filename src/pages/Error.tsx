ec32a590441ac9bf854ad965fb93604034e69ebf
Dasom Park <dev.somda@gmail.com>
2023-08-31 19:55:30 +0900

    FEAT: 에러 페이지 구현
    Co-authored-by: sjuhan123 <sjuhan123@gmail.com>
    Co-authored-by: hyunjinKim <joice.kim7@gmail.com>
    Co-authored-by: mihyunLee <mihy621@gmail.com>
    Co-authored-by: devseop <cocc263@gmail.com>

---
+++ src/pages/Error.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Error() {
+  return (
+    <div>
+      <h2>Error!</h2>
+      <a href='/'>Home으로 돌아가기</a>
+    </div>
+  );
+}
--- src/router/Routers.tsx
+++ src/router/Routers.tsx
@@ -5,6 +5,7 @@ import { RepoProvider } from '../contexts/repoContext';
 import Main from '../pages/Main';
 import IssueList from '../pages/IssueList';
 import Header from '../components/common/Header';
+import Error from '../pages/Error';
 
 export default function Routers() {
   return (
@@ -15,6 +16,7 @@ export default function Routers() {
           <Route element={<Header />}>
             <Route path={PATH.ISSUES} element={<IssueList />} />
           </Route>
+          <Route path='/*' element={<Error />} />
         </Routes>
       </RepoProvider>
     </BrowserRouter>
