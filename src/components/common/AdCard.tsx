a7b40281d5b04747c6ad13cad6d424da1553f58a
Dasom Park <dev.somda@gmail.com>
2023-08-31 22:47:22 +0900

    FEAT: 광고 이미지 주소 상수 분리
    Co-authored-by: sjuhan123 <sjuhan123@gmail.com>
    Co-authored-by: hyunjinKim <joice.kim7@gmail.com>
    Co-authored-by: mihyunLee <mihy621@gmail.com>
    Co-authored-by: devseop <cocc263@gmail.com>

--- src/components/common/AdCard.tsx
+++ src/components/common/AdCard.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
+import { AD } from '../../constants';
 
 export default function AdCard() {
   return (
     <AdImageContainer>
-      <AdImageWrapper href='https://www.wanted.co.kr/ '>
+      <AdImageWrapper href={AD.URL}>
         <AdText>[AD]</AdText>
-        <img
-          src='https://image.wanted.co.kr/optimize?src=https%3A%2F%2Fstatic.wanted.co.kr%2Fimages%2Fuserweb%2Flogo_wanted_black.png&w=110&q=100'
-          alt='ad-image'
-        />
+        <img src={AD.IMG} alt='ad-image' />
       </AdImageWrapper>
       <hr />
     </AdImageContainer>
