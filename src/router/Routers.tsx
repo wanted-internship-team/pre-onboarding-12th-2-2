ef3a4b8bdfd050905166f1d369c5ee738626ec8a
Dasom Park <dev.somda@gmail.com>
2023-08-31 15:55:04 +0900

    FEAT: repo 정보 전역 상태 관리
    Co-authored-by: Younseop Lee <devseop@users.noreply.github.com>
    Co-authored-by: Nor <mihyunLee@users.noreply.github.com>
    Co-authored-by: Den <sjuhan123@users.noreply.github.com>
    Co-authored-by: Joice(Hyunjin Kim) <JJineu@users.noreply.github.com>

--- src/router/Routers.tsx
+++ src/router/Routers.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { PATH } from '../constants';
+import { RepoProvider } from '../contexts/repoContext';
 import Main from '../pages/Main';
 
 export default function Routers() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path={PATH.MAIN} element={<Main />} />
-      </Routes>
+      <RepoProvider>
+        <Routes>
+          <Route path={PATH.MAIN} element={<Main />} />
+          <Route path={PATH.ISSUES} element={<Main />} />
+        </Routes>
+      </RepoProvider>
     </BrowserRouter>
   );
 }
