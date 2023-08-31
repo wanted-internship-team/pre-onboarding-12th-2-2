9b4c04033fac4552caf4c6f897faf9ab9294cc11
Dasom Park <dev.somda@gmail.com>
2023-08-31 19:07:12 +0900

    FEAT: 이슈 목록 무한 스크롤 구현
    Co-authored-by: Younseop Lee <devseop@users.noreply.github.com>
    Co-authored-by: Nor <mihyunLee@users.noreply.github.com>
    Co-authored-by: Den <sjuhan123@users.noreply.github.com>
    Co-authored-by: Joice(Hyunjin Kim) <JJineu@users.noreply.github.com>

--- src/pages/IssueList.tsx
+++ src/pages/IssueList.tsx
@@ -6,28 +6,46 @@ import { IissueList, IissueSummary } from '../types/issue';
 import { STATUS } from '../constants';
 import Spinner from '../components/common/Spinner';
 import IssueCard from '../components/IssueCard';
+import AdCard from '../components/common/AdCard';
+import useObserver from '../hooks/useObserver';
 
 export default function IssueList() {
   const [page, setPage] = useState(1);
+  const [issueList, setIssueList] = useState<IissueList>([]);
   const { repoInfo } = useRepoContext();
-  const { data, status, errorMessage, fetch } = useFetch<IissueList>(() =>
-    getRepoIssueList(repoInfo.owner, repoInfo.repo, page),
-  );
+  const { data, status, errorMessage, fetch } = useFetch<IissueList>();
+
+  const updatePage = () => {
+    setPage((prev) => prev + 1);
+  };
+
+  const refScroll = useObserver(updatePage, status);
+
+  useEffect(() => {
+    fetch({
+      callback: () => getRepoIssueList(repoInfo.owner, repoInfo.repo, page),
+    });
+  }, [page]);
+
+  useEffect(() => {
+    if (data) {
+      setIssueList((prev) => [...prev, ...data]);
+    }
+  }, [data]);
 
   return (
     <div>
       {status === STATUS.LOADING && <Spinner type='page' />}
-      {status === STATUS.ERROR && <p>에러!</p>}
-      {status === STATUS.SUCCESS &&
-        data &&
-        data.map((issue: IissueSummary) => (
+      {status === STATUS.ERROR && <p>{errorMessage}</p>}
+      {issueList &&
+        issueList.map((issue: IissueSummary, index: number) => (
           <React.Fragment key={issue.id}>
             <IssueCard issue={issue} />
             <hr />
+            {(index + 1) % 4 === 0 && <AdCard />}
           </React.Fragment>
         ))}
-
-      <div>이곳은 무한 스크롤 ref 만들 곳 </div>
+      <div ref={refScroll} />
     </div>
   );
 }
