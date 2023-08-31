66b350da88529e8e27179d39f16d8db5e667baff
Dasom Park <dev.somda@gmail.com>
2023-08-31 12:11:43 +0900

    FEAT: 중요 상수 설정
    Co-authored-by: sjuhan123 <sjuhan123@gmail.com>
    Co-authored-by: hyunjinKim <joice.kim7@gmail.com>
    Co-authored-by: mihyunLee <mihy621@gmail.com>
    Co-authored-by: devseop <cocc263@gmail.com>

--- src/router/Routers.tsx
+++ src/router/Routers.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { PATH } from '../constants';
 import Main from '../pages/Main';
 
 export default function Routers() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Main />} />
+        <Route path={PATH.MAIN} element={<Main />} />
       </Routes>
     </BrowserRouter>
   );
