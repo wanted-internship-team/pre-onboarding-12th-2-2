6cc0c0e94193f9ebd3bf125140106b4b378d380e
Dasom Park <dev.somda@gmail.com>
2023-08-31 19:05:57 +0900

    FEAT: 공통 헤더 컴포넌트 분리
    Co-authored-by: Younseop Lee <devseop@users.noreply.github.com>
    Co-authored-by: Nor <mihyunLee@users.noreply.github.com>
    Co-authored-by: Den <sjuhan123@users.noreply.github.com>
    Co-authored-by: Joice(Hyunjin Kim) <JJineu@users.noreply.github.com>

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import { useRepoContext } from '../../contexts/repoContext';
 import { Outlet } from 'react-router-dom';
+import { styled } from 'styled-components';
 
 export default function Header() {
   const { repoInfo } = useRepoContext();
 
   return (
     <>
-      <header>
-        <h2>
-          {repoInfo.owner}-{repoInfo.repo}
-        </h2>
-        <hr />
-      </header>
+      <HeaderContainer>
+        {repoInfo.owner} / {repoInfo.repo}
+      </HeaderContainer>
       <Outlet />
     </>
   );
 }
+
+const HeaderContainer = styled.header`
+  text-align: center;
+  font-size: 24px;
+`;
