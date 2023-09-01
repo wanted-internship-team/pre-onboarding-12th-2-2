1d922db9aca825052047ac23de9b7cba35ad0e20
sjuhan123 <sjuhan123@gmail.com>
2023-09-01 18:14:58 +0900

    FEAT: Octokit 라이브러리 응답 Error 대응 로직 구현
    - @octokit/request-error의 RequestError 인스턴스 활용해서 Error 대응 로직 구현
    - Error Message 내용 일부 수정

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -22,12 +22,14 @@ export const ERROR_MESSAGE = {
   USER: {
     404: '요청받은 페이지를 찾을 수 없습니다.',
     422: '잘못된 요청 형식으로 서버에서 해당 작업을 수행할 수 없습니다.',
-    DEFAULT: '서버 오류가 발생했습니다. 해당 오류가 지속적으로 발생한다면 관리자에게 문의해주세요.',
+    DEFAULT:
+      '서버 오류가 발생했습니다. 해당 오류가 지속적으로 발생한다면 관리자에게 문의해주세요\n 참고: https://docs.github.com/ko/rest/issues/issues?apiVersion=2022-11-28',
   },
   DEV: {
     404: 'Github API 에러메시지: Resource not found\n 참고: https://docs.github.com/ko/rest/issues/issues?apiVersion=2022-11-28',
     422: 'Github API 에러메시지: Validation failed, or the endpoint has been spammed.\n 참고: https://docs.github.com/ko/rest/issues/issues?apiVersion=2022-11-28',
-    DEFAULT: '서버 오류가 발생했습니다. 해당 오류가 지속적으로 발생한다면 관리자에게 문의해주세요.',
+    DEFAULT:
+      '서버 오류가 발생했습니다. 해당 오류가 지속적으로 발생한다면 관리자에게 문의해주세요.\n 참고: https://docs.github.com/ko/rest/issues/issues?apiVersion=2022-11-28',
   },
 } as const;
 
--- src/hooks/useFetch.tsx
+++ src/hooks/useFetch.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { ERROR_MESSAGE, STATUS } from '../constants';
 import { OctokitResponse } from '@octokit/types';
+import { RequestError } from '@octokit/request-error';
 
 type status = keyof typeof STATUS;
 
@@ -16,7 +17,6 @@ export default function useFetch<R>(callback?: () => Promise<OctokitResponse<R>>
   const fetch = async ({ callback }: Icallback<R>) => {
     try {
       if (!callback) return;
-
       setStatus(STATUS.LOADING);
 
       const res = await callback();
@@ -29,18 +29,19 @@ export default function useFetch<R>(callback?: () => Promise<OctokitResponse<R>>
       setStatus(STATUS.SUCCESS);
     } catch (error) {
       setStatus(STATUS.ERROR);
-
-      if (error instanceof Error) {
-        if (error.message === 'Not Found') {
-          // 에러코드 400
+      if (error instanceof RequestError) {
+        if (error.status === 404) {
           setErrorMessage(ERROR_MESSAGE.USER[404]);
           console.error(ERROR_MESSAGE.DEV[404]);
         }
-        if (error.message === 'Unprocessable Entity') {
-          // 에러코드 422
+        if (error.status === 422) {
           setErrorMessage(ERROR_MESSAGE.USER[422]);
           console.error(ERROR_MESSAGE.DEV[422]);
         }
+      } else {
+        // 500 에러 혹은 알 수 없는 에러
+        setErrorMessage(ERROR_MESSAGE.USER.DEFAULT);
+        console.error(ERROR_MESSAGE.DEV.DEFAULT);
       }
     }
   };
